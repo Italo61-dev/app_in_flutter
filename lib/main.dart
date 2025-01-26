7d44de1c50465ec4355eaa7ac693834951e08bc9
Italo61-dev <italosousa42541@gmail.com>
2025-01-25 21:46:10 -0300

    first stage

--- lib/main.dart
+++ lib/main.dart
@@ -15,62 +15,84 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Stack(
-            alignment: Alignment.center,
-            children: [
-              Container(
-                color: Colors.red,
-                width: 100,
-                height: 100,
+      home: Container(
+        color: Colors.white,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Container(
+                  color: Colors.red,
+                  width: 100,
+                  height: 100,
+                ),
+                Container(
+                  color: Colors.blue,
+                  width: 50,
+                  height: 50,
+                )
+              ],
+            ),
+            Stack(
+              alignment: Alignment.center,
+              children: [
+                Container(
+                  color: Colors.white,
+                  width: 100,
+                  height: 100,
+                ),
+                Container(
+                  color: Colors.red,
+                  width: 50,
+                  height: 50,
+                )
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Container(
+                  color: Colors.cyan,
+                  height: 50,
+                  width: 50,
+                ),
+                Container(
+                  color: Colors.pinkAccent,
+                  height: 50,
+                  width: 50,
+                ),
+                Container(
+                  color: Colors.purple,
+                  height: 50,
+                  width: 50,
+                ),
+              ],
+            ),
+            Container(
+              color: Colors.amber,
+              height: 30,
+              width: 300,
+              child: Text(
+                'Diamante Amarelo',
+                style: TextStyle(
+                  color: Colors.black,
+                  fontSize: 28,
+                ),
+                textAlign: TextAlign.center,
               ),
-              Container(
-                color: Colors.white,
-                width: 50,
-                height: 50,
-              )
-            ],
-          ),
-          Stack(
-            alignment: Alignment.center,
-            children: [
-              Container(
-                color: Colors.white,
-                width: 100,
-                height: 100,
-              ),
-              Container(
-                color: Colors.red,
-                width: 50,
-                height: 50,
-              )
-            ],
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Container(
-                color: Colors.cyan,
-                height: 50,
-                width: 50,
-              ),
-              Container(
-                color: Colors.pinkAccent,
-                height: 50,
-                width: 50,
-              ),
-              Container(
-                color: Colors.purple,
-                height: 50,
-                width: 50,
-              ),
-            ],
-          )
-        ],
+            ),
+            ElevatedButton(
+              onPressed: () {
+                print('Você apertou o botão');
+              },
+              child: Text('Aperte o botão!'),
+            )
+          ],
+        ),
       ),
     );
   }
