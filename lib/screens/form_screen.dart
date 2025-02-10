0282517a6e776e799c8787d00ee8482e35054857
Ítalo Nascimento <italosousa42541@gmail.com>
2025-02-10 20:04:29 -0300

    Update form_screen.dart

--- lib/screens/form_screen.dart
+++ lib/screens/form_screen.dart
@@ -35,6 +35,8 @@ class _FormScreenState extends State<FormScreen> {
             border: Border.all(width: 3),
           ),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Padding(
                 padding: const EdgeInsets.all(8.0),
@@ -67,7 +69,7 @@ class _FormScreenState extends State<FormScreen> {
                 child: TextFormField(
                   onChanged: (text) {
                     setState(() {
-                      
+
                     });
                   },
                   controller: imageController,
@@ -95,6 +97,9 @@ class _FormScreenState extends State<FormScreen> {
                   borderRadius: BorderRadius.circular(10),
                   child: Image.network(
                     imageController.text,
+                    errorBuilder: (BuildContext context, Object exception, StackTrace? stackTrace,){
+                      return Image.asset('assets/images/nophoto.png');
+                    },
                     fit: BoxFit.cover,
 
                   ),
