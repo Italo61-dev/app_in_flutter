a2b27621a2abc1082537ad246fb2a59df83c1ec0
Ítalo Nascimento <italosousa42541@gmail.com>
2025-03-03 23:06:51 -0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,33 @@
-import 'package:first_project/data/task_inherited.dart';
+
 import 'package:first_project/screens/form_screen.dart';
 import 'package:first_project/screens/initial_screen.dart';
 import 'package:flutter/material.dart';
 
-
 void main() {
-  runApp( const MyApp());
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-   const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: TaskInherited(child: const InitialScreen()),
-    );
-  }
+  State<MyApp> createState() => _MyAppState();
 }
 
+class _MyAppState extends State<MyApp> {
+  bool opacity = true;
+  @override
+  Widget build(BuildContext context) {
 
-
+    return MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: FormScreen(), //TaskInherited(child: const InitialScreen()),
+        );
+  }
+}
 
 
