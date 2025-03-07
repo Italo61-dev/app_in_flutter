af2a328d0cc63c3e7f83e935170c0f8b0e175771
Ítalo Nascimento <italosousa42541@gmail.com>
2025-03-06 23:08:28 -0300

    Update form_screen_test.dart

--- lib/screens/form_screen_test.dart
+++ lib/screens/form_screen_test.dart
@@ -11,7 +11,7 @@ class _FormScreenTestState extends State<FormScreenTest> {
   TextEditingController nameController = TextEditingController();
   TextEditingController difficultyController = TextEditingController();
   TextEditingController imageController = TextEditingController();
-  final _formkey = GlobalKey <FormState> ();
+  final _formkey = GlobalKey<FormState>();
 
   @override
   Widget build(BuildContext context) {
@@ -29,120 +29,129 @@ class _FormScreenTestState extends State<FormScreenTest> {
           ),
         ),
         body: Center(
-          child: Container(
-            width: 390,
-            height: 750,
-            decoration: BoxDecoration(
-              color: Colors.black12,
-              border: Border.all(width: 3),
-              borderRadius: BorderRadius.circular(8),
-            ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    validator: (String? value){
-                      if(value != null && value.isEmpty){
-                        return 'Insira o nome da tarefa';
-                      }
-                      return null;
-                    },
-                    keyboardType: TextInputType.name,
-                    textAlign: TextAlign.center,
-                    controller: nameController,
-                    decoration: InputDecoration(
-                      hintText: 'Nome',
-                      fillColor: Colors.white70,
-                      filled: true,
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
+          child: SingleChildScrollView(
+            child: Container(
+              width: 390,
+              height: 750,
+              decoration: BoxDecoration(
+                border: Border.all(
+                  width: 3,
                 ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    validator: (value){
-                      if(value!.isEmpty || int.parse(value) < 1 || int.parse(value) > 5){
-                        return 'Insira o nível de dificuldade entre 1 e 5!';
-                      }
-                      return null;
-                    },
-                    keyboardType: TextInputType.number,
-                    textAlign: TextAlign.center,
-                    controller: difficultyController,
-                    decoration: InputDecoration(
-                      hintText: 'Dificuldade',
-                      fillColor: Colors.white70,
-                      filled: true,
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: TextFormField(
-                    validator: (value){
-                      if(value!.isEmpty){
-                        return 'Insira uma URL';
-                      }
-                      return null;
-                    },
-                    keyboardType: TextInputType.url,
-                    textAlign: TextAlign.center,
-                    controller: imageController,
-                    onChanged: (text) {
-                      setState(() {
-      
-                      });
-                    },
-                    decoration: InputDecoration(
-                      hintText: 'Imagem',
-                      fillColor: Colors.white70,
-                      filled: true,
-                      border: OutlineInputBorder(),
-                    ),
-                  ),
-                ),
-                Container(
-                  width: 72,
-                  height: 100,
-                  decoration: BoxDecoration(
-                    color: Colors.red,
-                    border: Border.all(width: 2, color: Colors.blue),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.circular(8),
-                    child: Image.network(
-                      imageController.text,
-                      fit: BoxFit.cover,
-                      errorBuilder: (context, error, stackTrace) {
-                        return Image.asset(
-                          'assets/images/nophoto.png',
-                          fit: BoxFit.cover,
-                        );
+                borderRadius: BorderRadius.circular(8),
+                color: Colors.black12,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      validator: (String? value) {
+                        if (value != null && value.isEmpty) {
+                          return 'Insira o nome da tarefa!';
+                        }
+                        return null;
                       },
+                      keyboardType: TextInputType.text,
+                      controller: nameController,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        hintText: 'Nome',
+                        fillColor: Colors.white70,
+                        filled: true,
+                        border: OutlineInputBorder(),
+                      ),
                     ),
                   ),
-                ),
-                ElevatedButton(
-                  style: ElevatedButton.styleFrom(
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      validator: (value) {
+                        if (value!.isEmpty ||
+                            int.parse(value) < 1 ||
+                            int.parse(value) > 5) {
+                          return 'Insira um nível entre 1 a 5';
+                        }
+                        return null;
+                      },
+                      keyboardType: TextInputType.number,
+                      controller: difficultyController,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        hintText: 'Dificuldade',
+                        fillColor: Colors.white70,
+                        filled: true,
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: TextFormField(
+                      validator: (value){
+                        if(value!.isEmpty){
+                          return 'Insira uma Url válida';
+                        }
+                        return null;
+                      },
+                      keyboardType: TextInputType.url,
+                      onChanged: (text) {
+                        setState(() {});
+                      },
+                      controller: imageController,
+                      textAlign: TextAlign.center,
+                      decoration: InputDecoration(
+                        hintText: 'Imagem',
+                        fillColor: Colors.white70,
+                        filled: true,
+                        border: OutlineInputBorder(),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 72,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      border: Border.all(width: 2, color: Colors.blue),
+                      borderRadius: BorderRadius.circular(8),
+                      color: Colors.blue,
+                    ),
+                    child: ClipRRect(
+                      borderRadius: BorderRadius.circular(8),
+                      child: Image.network(
+                        imageController.text,
+                        errorBuilder: (context, error, stackTrace) {
+                          return Image.asset(
+                            'assets/images/nophoto.png',
+                            fit: BoxFit.cover,
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                  ElevatedButton(
+                    style: ElevatedButton.styleFrom(
                       backgroundColor: Colors.blue,
                       shape: RoundedRectangleBorder(
                         borderRadius: BorderRadius.circular(8),
-                      )),
-                  onPressed: () {
-                    if(_formkey.currentState!.validate()){}
-                  },
-                  child: Text(
-                    'Adicionar',
-                    style: TextStyle(color: Colors.white),
-                  ),
-                ),
-              ],
+                      ),
+                    ),
+                    onPressed: () {
+                      if (_formkey.currentState!.validate()) {
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(content: Text('Tarefa enviada')));
+                      }
+                    },
+                    child: Text(
+                      'Adicionar',
+                      style: TextStyle(
+                        color: Colors.white,
+                      ),
+                    ),
+                  )
+                ],
+              ),
             ),
           ),
         ),
